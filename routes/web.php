424024ed04d12e5e116fe45896f8ed2bd078457d
naufalamr17 <naufal.amr17@outlook.co.id>
2023-08-14 11:59:14 +0700

    Add: Check Sheet POWDER APAR

--- routes/web.php
+++ routes/web.php
@@ -40,11 +40,12 @@ Route::get('/dashboard/profile', [ProfileController::class, 'index'])->middlewar
 
 Route::resource('/dashboard/apar', AparController::class)->except('show')->middleware('auth');
 
-Route::get('/cekco2', function () {
-    return view('dashboard.checkCo2');
-});
-
 use App\Http\Controllers\CheckSheetCo2Controller;
 
 Route::get('/checksheetco2', [CheckSheetCo2Controller::class, 'index'])->name('checksheetco2.index');
 Route::post('/checksheetco2', [CheckSheetCo2Controller::class, 'store'])->name('checksheetco2.store');
+
+use App\Http\Controllers\CheckSheetPowderController;
+
+Route::get('/checksheetpowder', [CheckSheetPowderController::class, 'index'])->name('checksheetpowder.index');
+Route::post('/checksheetpowder', [CheckSheetPowderController::class, 'store'])->name('checksheetpowder.store');
