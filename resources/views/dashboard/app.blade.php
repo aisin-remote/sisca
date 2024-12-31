210d7375f618c832ae98c015ada58a4b14126daf
AIIA\fabian <mitsalf1@gmail.com>
2024-12-31 09:04:29 +0700

    localization

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -37,9 +37,7 @@
     <!-- Popper.JS -->
     <script src="{{ asset('dist/js/popper.min.js') }}" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
-    <script src="{{ asset('dist/js/bootstrap.min.js') }}"
-        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous">
-    </script>
+    <script src="{{ asset('dist/js/bootstrap.min.js') }}" crossorigin="anonymous"></script>
     {{-- Chart JS --}}
     <script src="{{ asset('dist/js/chart.js') }}"></script>
 
