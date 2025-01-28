f7381927ad7868166ae69ba4d74218109ac456aa
alessandrofhs <alessandrofhs@gmail.com>
2025-01-28 10:02:05 +0700

    update bug apar af11e

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -11,14 +11,8 @@
     <link rel="icon" href="/foto/aii.ico">
 
     {{-- CSS & JS Bootstrap --}}
-<<<<<<< HEAD
-    <link href="{{ asset('dist/css/bootstrap.min.css') }}" rel="stylesheet" crossorigin="anonymous">
-    <script src="{{ asset('dist/js/bootstrap.bundle.min.js') }}"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-=======
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
->>>>>>> 31c3bfa0dd42cc10e0d15cd01d685fecd048c616
     </script>
     <link rel="stylesheet" href="{{ asset('dist/css/bootstrap-icons.css') }}">
 
@@ -28,23 +22,6 @@
     <script src="/js/script.js"></script>
 
     <!-- Font Awesome JS -->
-<<<<<<< HEAD
-    <script defer src="{{ asset('dist/js/solid.js') }}"
-        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous">
-    </script>
-    <script defer src="{{ asset('dist/js/fontawesome.js') }}"
-        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous">
-    </script>
-
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="{{ asset('dist/js/jquery-3.3.1.slim.min.js') }}"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <!-- Popper.JS -->
-    <script src="{{ asset('dist/js/popper.min.js') }}" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script src="{{ asset('dist/js/bootstrap.min.js') }}" crossorigin="anonymous"></script>
-=======
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js">
     </script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js">
@@ -59,7 +36,6 @@
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js">
     </script>
->>>>>>> 31c3bfa0dd42cc10e0d15cd01d685fecd048c616
     {{-- Chart JS --}}
     <script src="{{ asset('dist/js/chart.js') }}"></script>
 
